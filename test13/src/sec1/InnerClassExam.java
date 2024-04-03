c2c25c59913c330a9d9cbf81278cad342990497e
blackbird0220 <kewchb@naver.com>
2024-04-03 18:29:03 +0900

    java class & eclipse 2023

--- test13/src/sec1/InnerClassExam.java
+++ test13/src/sec1/InnerClassExam.java
@@ -1,5 +1,5 @@
 package sec1;
-
+//람다(Lambda) : 메소드를 더 간소화한 표현식 /점점 많이 쓰임
 public class InnerClassExam {
 
 	public static void main(String[] args) {
--- test13/src/sec3/StreamExam.java
+++ test13/src/sec3/StreamExam.java
@@ -1,5 +1,5 @@
 package sec3;
-
+//Stream : 입출력 방식의 제어
 public class StreamExam {
 
 	public static void main(String[] args) {
